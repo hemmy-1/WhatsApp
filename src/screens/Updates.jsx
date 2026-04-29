4b15b42005105a7418eb08b54ae49d331881c63f
Victor Abass <owooluwaabass3@gmail.com>
2026-04-29 11:56:45 +0100

    Refactor Updates component for improved readability and consistency

--- src/screens/Updates.jsx
+++ src/screens/Updates.jsx
@@ -1,42 +1,43 @@
-import { Text, View,StyleSheet, } from "react-native";
-import { SafeAreaProvider,SafeAreaView, } from "react-native-safe-area-context";
+import { Text, View, StyleSheet, } from "react-native";
+import { SafeAreaProvider, SafeAreaView, } from "react-native-safe-area-context";
 import EvilIcons from '@expo/vector-icons/EvilIcons';
 import Entypo from '@expo/vector-icons/Entypo';
 
 
 
 export default function Updates() {
-    return(
+    return (
         <SafeAreaProvider>
             <SafeAreaView style={styles.container}>
 
-                <View style={{flexDirection:'row', justifyContent:'space-between', alignItems:'center', padding:10,}}>
-            <View>
-            <Text style={{fontSize:40,color:'white',}}>
-                Updates
-            </Text>
-            </View>
+                <View style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', padding: 10, }}>
+                    <View>
+                        <Text style={{ fontSize: 40, color: 'white', }}>
+                            Updates
+                        </Text>
+                    </View>
 
-            <View style={{flexDirection:'row', gap:20,}}>
-             <EvilIcons name="search" size={24} color="white" />
-             <Entypo name="dots-three-vertical" size={24} color="white" />
-            </View>
-        </View>
+                    <View style={{ flexDirection: 'row', gap: 20, }}>
+                        <EvilIcons name="search" size={24} color="white" />
+                        <Entypo name="dots-three-vertical" size={24} color="white" />
+                    </View>
+                </View>
 
-        <View style={{width:"95%",height:50,backgroundColor:"#0a332b",borderRadius:10,justifyContent:'center',alignItems:'center',margin:10,
-            borderRadius:10,
-        }}>
-           
-            <Text style={{color:'white', }}>
-                Discover more with relevant ads only in Status &{'\n'}Channels.
-            <Text style={{color:"green",}}>
-                See details and updated terms
-            </Text>
-            </Text>   
-        </View>
+                <View style={{
+                    width: "95%", height: 50, backgroundColor: "#0a332b", borderRadius: 10, justifyContent: 'center', alignItems: 'center', margin: 10,
+                    borderRadius: 10,
+                }}>
 
-<Text style={{color:"white"}}>Status</Text>
-        </SafeAreaView>
+                    <Text style={{ color: 'white', }}>
+                        Discover more with relevant ads only in Status &{'\n'}Channels.
+                        <Text style={{ color: "green", }}>
+                            See details and updated terms
+                        </Text>
+                    </Text>
+                </View>
+
+                <Text style={{ color: "white" }}>Status</Text>
+            </SafeAreaView>
         </SafeAreaProvider>
     )
 }
@@ -45,7 +46,7 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: '#0b1014',
-       paddingTop: 10,
-        
+        paddingTop: 10,
+
     }
 });
