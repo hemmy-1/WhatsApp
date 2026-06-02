e486eecce511527135b6d98965fe9754fd7d6aa7
Adegbola Emmanuel <princeadekunle2001@gmail.com>
2026-06-02 15:30:30 +0100

    updated the toast

--- App.js
+++ App.js
@@ -1,14 +1,13 @@
 import { NavigationContainer } from "@react-navigation/native";
 import MainNavigator from "./src/navigator/MainNavigator";
 import Toast from 'react-native-toast-message';
-import { ToastProvider } from "./src/utils/utils";
 
 
 export default function App() {
   return(
     <NavigationContainer>
       <MainNavigator/>
-      <ToastProvider />
+      <Toast />
     </NavigationContainer>
   )
 }
